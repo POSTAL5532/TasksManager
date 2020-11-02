286bd2aeb902edb7c3676344a3213a97d2a14a1a
Scherbachenya, Igor <evgeniionegin791@gmail.com>
2020-11-02 21:01:33 +0300

    Add test data

--- tasks-manager-data/src/scripts/addTestData.sql
+++ tasks-manager-data/src/scripts/addTestData.sql
@@ -1,8 +1,24 @@
 INSERT INTO users (id, first_name, last_name, email, password, role, reg_date, status, confirm_status)
 VALUES ('4028e48f713215020171321690280001', 'Igor', 'Shcherbachenya', 'postal@qwe.qwe',
-        '$2a$10$jb8iMtrfoEgbjwiRgdlKl.sM32taqs/bXnbD7nvDL6PON/dEt8owW',
+        '$2a$10$daTJ4qjtST0oYw48qP0EweI9sR9x8Wjrgzty5nbEfCbmytrfcoaiO',
         'ROLE_USER', '2020-09-05', 'ACTIVE', 'CONFIRMED'),
 
        ('4028e48f713215020171321690280002', 'Ivan', 'Yablochkin', 'ivan@qwe.qwe',
-        '$2a$10$NeaQzrm9YhXv8tB054QQ4.QjRWyWYczf4myTGGxNmyxOboBe93Zoa',
-        'ROLE_USER', '2020-05-25', 'ACTIVE', 'UNCONFIRMED');
+        '$2a$10$daTJ4qjtST0oYw48qP0EweI9sR9x8Wjrgzty5nbEfCbmytrfcoaiO',
+        'ROLE_USER', '2020-03-23', 'ACTIVE', 'UNCONFIRMED'),
+
+       ('4028e48f713215020171321690280003', 'Alex', 'Simonov', 'alex@qwe.qwe',
+        '$2a$10$daTJ4qjtST0oYw48qP0EweI9sR9x8Wjrgzty5nbEfCbmytrfcoaiO',
+        'ROLE_USER', '2020-04-24', 'BANNED', 'CONFIRMED'),
+
+       ('4028e48f713215020171321690280004', 'Yuri', 'Danilov', 'yuri@qwe.qwe',
+        '$2a$10$daTJ4qjtST0oYw48qP0EweI9sR9x8Wjrgzty5nbEfCbmytrfcoaiO',
+        'ROLE_USER', '2020-02-22', 'DELETED', 'CONFIRMED'),
+
+       ('4028e48f713215020171321690280005', 'Evgeniy', 'Otetcovich', 'evgen@qwe.qwe',
+        '$2a$10$daTJ4qjtST0oYw48qP0EweI9sR9x8Wjrgzty5nbEfCbmytrfcoaiO',
+        'ROLE_USER', '2020-05-25', 'BANNED', 'UNCONFIRMED'),
+
+       ('4028e48f713215020171321690280006', 'Tatiana', 'Mamovna', 'Tatiana@qwe.qwe',
+        '$2a$10$daTJ4qjtST0oYw48qP0EweI9sR9x8Wjrgzty5nbEfCbmytrfcoaiO',
+        'ROLE_USER', '2020-09-18', 'DELETED', 'UNCONFIRMED');
